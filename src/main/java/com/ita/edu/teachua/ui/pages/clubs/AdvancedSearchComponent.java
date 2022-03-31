90720eb929fffe582177b4a01f5c27ba4c2c458c
Kateryna Mushynska <kate080227@gmail.com>
2022-03-31 12:26:03 +0300

    Added TUA-513

--- src/main/java/com/ita/edu/teachua/ui/pages/clubs/AdvancedSearchComponent.java
+++ src/main/java/com/ita/edu/teachua/ui/pages/clubs/AdvancedSearchComponent.java
@@ -5,9 +5,23 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import java.util.List;
+
 public class AdvancedSearchComponent extends CommonPage {
     @FindBy(how = How.XPATH, using = "//span[text()='Центр']")
     private WebElement centerButton;
+
+    @FindBy(how = How.XPATH, using = "//label[@class='ant-radio-button-wrapper club-view-button']/span/img")
+    private WebElement listIcon;
+
+    @FindBy(how = How.XPATH, using = "//div[contains(@class,'content-center-list')]/child::div")
+    private List<WebElement> centers;
+
+    @FindBy(how = How.XPATH, using = "//ul/li[contains(@class, 'ant-pagination-item')]")
+    private List<WebElement> listOfPaginationButtons;
+
+    @FindBy(how = How.XPATH, using = "//li[@title='Next Page']")
+    private WebElement nextPageButton;
 
     public AdvancedSearchComponent(WebDriver driver) {
         super(driver);
@@ -15,7 +29,30 @@ public class AdvancedSearchComponent extends CommonPage {
 
     public AdvancedSearchComponent clickOnСenterButton() {
         centerButton.click();
+        sleep(3000);
         return this;
+    }
+
+    public AdvancedSearchComponent clickOnListIcon() {
+        listIcon.click();
+        sleep(4000);
+        return this;
+    }
+
+    public boolean isCentersDisplayedAsAList(int expectedWidth, int expectedHeight) {
+        for (WebElement numberOfPageButton : listOfPaginationButtons) {
+            for (WebElement element : centers) {
+                if (!(element.getSize().height == expectedHeight && element.getSize().width == expectedWidth)) {
+                    return false;
+                }
+                sleep(4000);
+            }
+            if (numberOfPageButton.getAttribute("class").contains("active")) {
+                nextPageButton.click();
+                sleep(4000);
+            }
+        }
+        return true;
     }
 
     public boolean isDisplayedCategory(String nameOfCategory) {
--- src/test/java/com/ita/edu/teachua/ui/tests/AdvancedSearchTest.java
+++ src/test/java/com/ita/edu/teachua/ui/tests/AdvancedSearchTest.java
@@ -5,10 +5,12 @@ import com.ita.edu.teachua.ui.pages.home.HomePage;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class AdvancedSearchTest extends TestRunner{
+import static org.testng.Assert.assertTrue;
+
+public class AdvancedSearchTest extends TestRunner {
 
     @Test(description = "TUA-510")
-    public void verifyThatParametersDeactivatedAfterSelectingCenterBtn(){
+    public void verifyThatParametersDeactivatedAfterSelectingCenterBtn() {
         SoftAssert softAssert = new SoftAssert();
         HomePage home = new HomePage(driver);
         AdvancedSearchComponent advancedSearchComponent = home.getHeader()
@@ -30,5 +32,18 @@ public class AdvancedSearchTest extends TestRunner{
                 .isDisplayedCategory("Вік дитини"));
 
         softAssert.assertAll();
+    }
+
+    @Test(description = "TUA-513")
+    public void verifyThatCentersDisplayedAsAList() {
+        int expectedHeight = 184;
+        int expectedWidth = 944;
+        HomePage home = new HomePage(driver);
+        AdvancedSearchComponent advancedSearchComponent = home.getHeader()
+                .clickAdvancedSearchBtn()
+                .clickOnСenterButton()
+                .clickOnListIcon();
+
+        assertTrue(advancedSearchComponent.isCentersDisplayedAsAList(expectedWidth, expectedHeight));
     }
 }
