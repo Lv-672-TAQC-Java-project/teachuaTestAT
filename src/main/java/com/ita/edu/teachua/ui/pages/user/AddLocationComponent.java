c10ec63fbc41b93f3070254d922ea00bcd4c6adc
Ira Rybak <ira.rybak17@gmail.com>
2022-04-11 08:39:42 +0300

    verify that user can create enter

--- src/main/java/com/ita/edu/teachua/ui/pages/user/AddLocationComponent.java
+++ src/main/java/com/ita/edu/teachua/ui/pages/user/AddLocationComponent.java
@@ -1,7 +1,10 @@
 package com.ita.edu.teachua.ui.pages.user;
 
 import com.ita.edu.teachua.ui.pages.base.BasePage;
+import com.ita.edu.teachua.ui.pages.user.addcenter.BasicInformationCenterComponent;
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,9 +14,139 @@ public class AddLocationComponent extends BasePage {
     @FindBy(how = How.XPATH, using = "/html/body/div[6]/div/div[2]/div/div[2]/div/main/div[2]/form/div[6]/button")
     private WebElement addButton;
 
+    @FindBy(how = How.XPATH, using = "//div[contains(@class, 'add-location-button')]//button[@type = 'submit']")
+    private WebElement addLocationButton;
+
+    @FindBy(how = How.ID, using = "name")
+    private WebElement locationNameField;
+
+    @FindBy(how = How.ID, using = "address")
+    private WebElement addressField;
+
+    @FindBy(how = How.ID, using = "coordinates")
+    private WebElement coordinatesField;
+
+    @FindBy(how = How.ID, using = "phone")
+    private WebElement phoneField;
+
+    @FindBy(how = How.ID, using = "cityName")
+    private WebElement cityListButton;
+
+    @FindBy(how = How.ID, using = "districtName")
+    private WebElement districtListButton;
+
+    @FindBy(how = How.ID, using = "stationName")
+    private WebElement stationListButton;
+
+    @FindBy(how = How.XPATH, using = "/html/body/div[6]/div/div/div/div[2]/div[1]/div/div")
+    private WebElement locationList;
+
+    private String checkCirclePath = "./..//*[@aria-label ='check-circle']";
+
     public AddLocationComponent(WebDriver driver) {
         super(driver);
     }
+
+
+    @Step("input location name {locationName}")
+    public AddLocationComponent inputLocationNameField(String locationName) {
+        locationNameField.sendKeys(locationName);
+        return new AddLocationComponent(driver);
+    }
+
+    @Step("Check if Data in name field is displayed")
+    public boolean isCheckCircleOfLocationNameFieldDisplayed() {
+        return locationNameField.findElement(By.xpath(checkCirclePath)).isDisplayed();
+    }
+
+    @Step("input address {address}")
+    public AddLocationComponent inputAddressField(String address) {
+        addressField.sendKeys(address);
+        return new AddLocationComponent(driver);
+    }
+
+    @Step("Check if address is displayed")
+    public boolean isCheckCircleOfLocationAddressFieldDisplayed() {
+        return addressField.findElement(By.xpath(checkCirclePath)).isDisplayed();
+    }
+
+    @Step("input coordinates {coordinates}")
+    public AddLocationComponent inputCoordinatesField(String coordinates) {
+        coordinatesField.sendKeys(coordinates);
+        return new AddLocationComponent(driver);
+    }
+
+    @Step("Check if coordinates is displayed")
+    public boolean isCheckCircleOfLocationCoordinatesFieldDisplayed() {
+        return coordinatesField.findElement(By.xpath(checkCirclePath)).isDisplayed();
+    }
+
+    @Step("input phone {phone}")
+    public AddLocationComponent inputPhoneField(String phone) {
+        phoneField.sendKeys(phone);
+        return new AddLocationComponent(driver);
+    }
+
+    @Step("Check if phone number is displayed")
+    public boolean isCheckCircleOfLocationPhoneFieldDisplayed() {
+        return phoneField.findElement(By.xpath(checkCirclePath)).isDisplayed();
+    }
+
+    public AddLocationComponent clickCityListButton() {
+        cityListButton.click();
+        sleep(1000);
+        return new AddLocationComponent(driver);
+    }
+
+    public AddLocationComponent clickDistrictListButton() {
+        districtListButton.click();
+        sleep(1000);
+        return new AddLocationComponent(driver);
+    }
+
+    public AddLocationComponent clickStationListButton() {
+        stationListButton.click();
+        sleep(1000);
+        return new AddLocationComponent(driver);
+    }
+
+    @Step("Verify that listBox contain {location}")
+    public boolean isElementFromDropDownListDisplayed(String location) {
+        String str = String.format(".//div[text() = '%s']", location);
+        try {
+            boolean isContainsLocation = locationList.findElement(By.xpath(str)).isDisplayed();
+            return isContainsLocation;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+
+    }
+
+    @Step("select {location} element from the drop-down list")
+    public AddLocationComponent clickElementFromDropDownList(String location) {
+
+        String str = String.format(".//div[text() = '%s']", location);
+        locationList.findElement(By.xpath(str)).click();
+
+        return new AddLocationComponent(driver);
+    }
+
+    @Step("check is Add Button Enable ")
+    public boolean isAddLocationButtonEnable() {
+        return addLocationButton.isEnabled();
+    }
+
+    @Step("add location")
+    public BasicInformationCenterComponent clickAddLocationButton() {
+        addLocationButton.click();
+        sleep(2000);
+        return new BasicInformationCenterComponent(driver);
+    }
+
+    public boolean isAddLocationButtonEnabled() {
+        return addLocationButton.isEnabled();
+    }
+
     @Step("check is Add Button Enable ")
     public boolean isAddButtonEnable() {
         return addButton.isEnabled();
---
+++ src/test/java/com/ita/edu/teachua/ui/tests/AdminAddCenterTest.java
@@ -0,0 +1,135 @@
+package com.ita.edu.teachua.ui.tests;
+
+import com.ita.edu.teachua.ui.pages.home.HomePage;
+import com.ita.edu.teachua.ui.pages.user.AddLocationComponent;
+import com.ita.edu.teachua.ui.pages.user.MyProfilePage;
+import com.ita.edu.teachua.ui.pages.user.addcenter.BasicInformationCenterComponent;
+import com.ita.edu.teachua.ui.pages.user.addcenter.ContactsCenterComponent;
+import com.ita.edu.teachua.ui.pages.user.addcenter.DescriptionCenterComponent;
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class AdminAddCenterTest extends TestRunnerWithValueProvider {
+    @BeforeMethod
+    public void beforeAddCenter() {
+        HomePage homePage = new HomePage(driver);
+        homePage.getHeader().login(valueProvider.getAdminEmail(), valueProvider.getAdminPassword());
+    }
+
+    final String CITY = "Київ";
+    final String DISTRICT = "Деснянський";
+    final String STATION = "Академістечко";
+
+    @Description("Verify optional fields work correctly")
+    @Issue("TUA-214")
+    @Test(description = "TUA-214")
+    public void verifyThatOptionalFieldsAreDisplayed() {
+
+        MyProfilePage myProfilePage = new MyProfilePage(driver);
+        myProfilePage
+                .getHeader()
+                .clickAdminProfile()
+                .clickMyProfileButton()
+                .clickAddButton()
+                .clickAddCenterInDropDownButton();
+
+        SoftAssert softAssert = new SoftAssert();
+
+        AddLocationComponent addLocationComponent = new AddLocationComponent(driver);
+        softAssert.assertTrue(addLocationComponent
+                .clickCityListButton()
+                .isElementFromDropDownListDisplayed(CITY), CITY + " should be displayed in drop down list");
+        addLocationComponent.clickElementFromDropDownList(CITY);
+
+        softAssert.assertTrue(addLocationComponent
+                .clickDistrictListButton()
+                .isElementFromDropDownListDisplayed(DISTRICT), DISTRICT + " should be displayed in drop down list");
+        addLocationComponent.clickElementFromDropDownList(DISTRICT);
+
+        softAssert.assertTrue(addLocationComponent
+                .clickStationListButton()
+                .isElementFromDropDownListDisplayed(STATION),STATION + " should be displayed in drop down list" );
+        addLocationComponent.clickElementFromDropDownList(STATION);
+
+        softAssert.assertAll();
+    }
+
+    @Description("[Center] Verify that user can create a center with valid data")
+    @Issue("TUA-214")
+    @Test(description = "TUA-214")
+    public void VerifyThatUserCanCreateCenterWithValidData()  {
+        SoftAssert softAssert = new SoftAssert();
+
+        MyProfilePage myProfilePage = new MyProfilePage(driver);
+        AddLocationComponent addLocationComponent = new AddLocationComponent(driver);
+        myProfilePage
+                .getHeader()
+                .clickAdminProfile()
+                .clickMyProfileButton()
+                .clickAddButton()
+                .clickAddCenterInDropDownButton();
+
+        softAssert.assertTrue(myProfilePage.addCenterButtonIsEnabled());
+
+        BasicInformationCenterComponent basicInformationCenterComponent = new BasicInformationCenterComponent(driver);
+
+        softAssert.assertTrue(basicInformationCenterComponent
+                .inputNameCenterField("Center#1")
+                .getFieldNameCenter().isEmpty(), "The name of the center should be filled");
+
+        basicInformationCenterComponent.clickAddLocationButton();
+
+        addLocationComponent
+                .inputLocationNameField("test1")
+                .clickCityListButton()
+                .clickElementFromDropDownList(CITY)
+                .inputAddressField("Мазепи 55")
+                .inputCoordinatesField("49.829104498711104, 24.005058710351314")
+                .inputPhoneField("0938784576");
+
+        softAssert.assertTrue(addLocationComponent.isCheckCircleOfLocationNameFieldDisplayed(),
+                "the location name is entered correctly");
+        softAssert.assertTrue(addLocationComponent.isElementFromDropDownListDisplayed(CITY),
+                CITY + " should be displayed in drop down list");
+        softAssert.assertTrue(addLocationComponent.isCheckCircleOfLocationAddressFieldDisplayed(),
+                "the address of location is entered correctly");
+        softAssert.assertTrue(addLocationComponent.isCheckCircleOfLocationCoordinatesFieldDisplayed(),
+                "the coordinates of location is entered correctly");
+        softAssert.assertTrue(addLocationComponent.isCheckCircleOfLocationPhoneFieldDisplayed(),
+                "the phone number of location is entered correctly");
+
+        addLocationComponent.clickAddLocationButton();
+        softAssert.assertTrue(addLocationComponent.isAddLocationButtonEnable());
+
+        basicInformationCenterComponent
+                .clickSelectedLocations(new String[]{"Школярик001"})
+                .clickNextButton();
+
+        ContactsCenterComponent contactsCenterComponent = new ContactsCenterComponent(driver);
+
+        contactsCenterComponent
+                .inputPhoneField("0950000000").isCheckCircleOfPhoneNumberFieldDisplayed();
+
+        softAssert.assertTrue(contactsCenterComponent.isCheckCircleOfPhoneNumberFieldDisplayed(),
+                "the phone number is entered correctly");
+        contactsCenterComponent
+                .clickNextButton();
+
+        DescriptionCenterComponent descriptionCenterComponent = new DescriptionCenterComponent(driver);
+
+        descriptionCenterComponent
+                .inputDescriptionField("test test test test test test test test test");
+
+        softAssert.assertTrue(descriptionCenterComponent.isCheckCircleOfDescriptionFieldDisplayed(),
+                "the Description is entered correctly");
+        descriptionCenterComponent
+                .clickNextButton()
+                .clickSelectedClubs(new String[]{"test123456789"})
+                .clickFinishButton();
+
+        softAssert.assertAll();
+    }
+}
