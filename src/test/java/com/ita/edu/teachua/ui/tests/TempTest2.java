91e8cc7bbb04fec790cb018be1d3de1d499337da
Veglok <Qwertywo2000@gmail.com>
2022-04-18 22:34:19 +0300

    72

--- src/test/java/com/ita/edu/teachua/ui/tests/TempTest2.java
+++ src/test/java/com/ita/edu/teachua/ui/tests/TempTest2.java
@@ -1,11 +1,7 @@
 package com.ita.edu.teachua.ui.tests;
 
 
-import com.ita.edu.teachua.utils.jdbc.entity.ChallengesEntity;
-import com.ita.edu.teachua.utils.jdbc.entity.NewsEntity;
 import com.ita.edu.teachua.utils.jdbc.entity.UserEntity;
-import com.ita.edu.teachua.utils.jdbc.services.ChallengesService;
-import com.ita.edu.teachua.utils.jdbc.services.NewsService;
 import com.ita.edu.teachua.utils.jdbc.services.UserService;
 import org.testng.annotations.Test;
 
@@ -36,23 +32,6 @@ public class TempTest2 {
         for (UserEntity user : users) {
             System.out.println(user);
         }
-    }
 
-    @Test
-    public void Challenges() {
-        ChallengesService challengesService = new ChallengesService();
-        List<ChallengesEntity> challenges = challengesService.getAllChallenges();
-        for (ChallengesEntity challenge : challenges) {
-            System.out.println(challenge);
-        }
-    }
-
-    @Test
-    public void News() {
-        NewsService newsService = new NewsService();
-        List<NewsEntity> news = newsService.getAllNews();
-        for (NewsEntity newsItem : news) {
-            System.out.println(newsItem);
-        }
     }
 }
