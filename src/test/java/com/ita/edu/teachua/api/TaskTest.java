df084d3b54ecb3882a50415e049c10bee9910b33
DianaSt123 <diana.stoyko@gmail.com>
2022-05-18 14:42:39 +0300

    TUA-444

--- src/test/java/com/ita/edu/teachua/api/TaskTest.java
+++ src/test/java/com/ita/edu/teachua/api/TaskTest.java
@@ -38,4 +38,23 @@ public class TaskTest extends ApiTestRunner {
         softAssert.assertEquals(response.getStatusCode(), 200);
         softAssert.assertAll();
     }
+
+    @Description("Verify that user can edit Task with valid values.")
+    @Issue("TUA-444")
+    @Test(description = "TUA-444")
+    public void verifyThatUserCanEditTaskWithValidValues() {
+
+        TaskCredentials taskCredentials = new TaskCredentials(" namenamename1213#$% ",
+                "stringstringstringstringstringstringstringstring",
+                " descriptiondescriptiondescriptiondescriptiondescription12345$%%^$# ",
+                "/upload/test/test.png",
+                "2022-05-25",
+                224L);
+
+        var response = task.put(310, taskCredentials);
+
+        var softAssert = new SoftAssert();
+        softAssert.assertEquals(response.getStatusCode(), 200);
+        softAssert.assertAll();
+    }
 }
